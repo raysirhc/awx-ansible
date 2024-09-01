252c4fcc35bed3eb92aafc01b208b8a145e89959
raysirhc <yar_yu_xiang@hotmail.com>
2024-09-01 19:34:40 +0800

    Create APIIntegration.sh

---
+++ Windows/APIIntegration.sh
@@ -0,0 +1,1 @@
+curl -d '{"VariableValue1": "flyinguser","VariableValue2": "local.domain"}' -H "Content-Type: application/json" -H "HeaderValue: HeaderKey" -X POST https://FQDN/api/integrations/randomstring
