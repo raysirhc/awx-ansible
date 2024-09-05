6305b7090a3a3f98bd617222e3e328c97e7ef23f
raysirhc <yar_yu_xiang@hotmail.com>
2024-09-05 20:39:51 +0800

    Create api.ps1

---
+++ Windows/api.ps1
@@ -0,0 +1,12 @@
+#powershell code
+
+$URL = "https://awx.poco.stream/api/integrations/feujrfonyzdbu6g5"
+$JSON_FILE = "C:\Users\yaryu\data.json"
+
+$Body = Get-Content -Raw -Path $JSON_FILE
+$Headers = @{ 
+"Content-Type" = "application/json"
+"YYX" = "YYXAPI" 
+}
+
+Invoke-RestMethod -Uri $URL -Method Post -Headers $Headers -Body $Body
